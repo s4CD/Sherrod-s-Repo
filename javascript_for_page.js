0ddba9229cbcc41d767c6e7c5f79d0c9f50989f2
Sherrod Jenkins <=>
2018-01-16 17:10:31 -0500

    First Commit

---
+++ javascript_for_page.js
@@ -0,0 +1,27 @@
+/*
+
+Here is my Javascript
+Sherrod Jenkins
+January 16, 2018
+
+*/
+
+function function1()
+{
+	
+}
+
+function function2()
+{
+
+}
+
+function function3()
+{
+
+}
+
+function funtion4()
+{
+
+}
